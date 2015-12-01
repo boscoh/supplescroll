dd18d32ba498d54ad8a51ea3a9c03563c368db20
Bosco Ho <apposite@gmail.com>
2015-12-01 15:03:07 +1100

    fixed python version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-version = open('supplescroll/_version.py').read().split()[-1][1:-1]
+version = open('_version.py').read().split()[-1][1:-1]
 
 setup(
     name='supplescroll',
